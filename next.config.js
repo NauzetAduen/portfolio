c2e7ea09b00540dcde4e030a143f199d897f9a6b
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-10 00:02:32 +0100

    ADD SASS and create  basic folder structure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
 module.exports = {
   reactStrictMode: true,
-}
+};
