6e88227389d8c9f0932fbcc485c2414f7f985290
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-10 01:13:33 +0100

    ADD jsx extension

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import Hero from "../components/Hero";
-import Styles from "../styles/Home.module.scss";
+import styles from "../styles/Home.module.scss";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -11,10 +11,11 @@ export default function Home() {
         <meta name="description" content="Portfolio by Nauzet Hernández" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={Styles.fullHeight}>
+      <Nav />
+      <div className={styles.fullHeight}>
         <Hero />
       </div>
-      <div className={Styles.fullHeight}>cocaina</div>
+      <div className={styles.fullHeight}>cocaina</div>
     </>
   );
 }
---
+++ pages/me.jsx
@@ -0,0 +1,3 @@
+export default function me() {
+  return <div>me</div>;
+}
