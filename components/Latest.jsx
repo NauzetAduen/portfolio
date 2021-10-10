66d74010b6d28a1dce59b8d3447d75402298815c
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-10 19:28:44 +0100

    ADD placeholders and see more button

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -6,12 +6,35 @@ export default function Latest() {
         <p className={styles.title}>Latest creations</p>
       </div>
       <div className={styles.creation}>
-        <div className={styles.subcreation}>a</div>
-        <div className={styles.subcreation}>b</div>
+        <div className={styles.subcreation}>
+          <div className={styles.placeholder}>Image</div>
+        </div>
+        <div className={styles.subcreation}>
+          <p className={styles.creationTitle}>
+            Lorem ipsum whatevah Error sintarsis
+          </p>
+          <p>
+            Lorem ipsum whatevah Error sintarsis, Lorem ipsum whatevah Error
+            sintarsis, Lorem ipsum whatevah Error sintarsis , Lorem ipsum
+            whatevah Error sintarsis, Lorem ipsum whatevah Error sintarsis. 123.
+          </p>
+        </div>
       </div>
-      <div className={styles.creation}>
-        <div className={styles.subcreation}>a</div>
-        <div className={styles.subcreation}>b</div>
+      <div className={styles.creationReverse}>
+        <div className={styles.subcreation}>
+          <p className={styles.creationTitle}>Second Lorem Ipsun</p>
+          <p>
+            Lorem ipsum whatevah Error sintarsis, Lorem ipsum whatevah Error
+            sintarsis, Lorem ipsum whatevah Error sintarsis , Lorem ipsum
+            whatevah Error sintarsis, Lorem ipsum whatevah Error sintarsis. 123.
+          </p>
+        </div>
+        <div className={styles.subcreation}>
+          <div className={styles.placeholder}>Image</div>
+        </div>
+      </div>
+      <div className={styles.seeMore}>
+        <button>See more</button>
       </div>
     </div>
   );
