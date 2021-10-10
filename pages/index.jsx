0cdd401e34ca3f6ccab40121a7dbb028e36a026a
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-10 21:53:44 +0100

    ADD footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import Hero from "../components/Hero";
 import styles from "../styles/Home.module.scss";
 import Nav from "../components/Nav";
 import Latest from "../components/Latest";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -18,6 +20,12 @@ export default function Home() {
       </div>
       <div className={styles.fullHeight}>
         <Latest />
+      </div>
+      <div className={styles.fullHeight}>
+        <Contact />
+      </div>
+      <div className={styles.halfHeight}>
+        <Footer />
       </div>
     </>
   );
