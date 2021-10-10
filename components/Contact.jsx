fd3d294f8783f0ecbcc3994272fea6f795b691e7
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-10 21:53:37 +0100

    ADD contact section

---
+++ components/Contact.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/Contact.module.scss";
+
+export default function Contact() {
+  return (
+    <div className={styles.contact}>
+      <div>
+        <p className={styles.title}>Now that we're here</p>
+      </div>
+      <div className={styles.welcomeDiv}>
+        <div className={styles.welcomeText}>
+          Thank you for coming home. Now you should check my work and, possibly,
+          my blog. After your visit, contact me if you please.
+        </div>
+        <div className={styles.personalCard}>
+          <div className={styles.placeholder}>placeholder</div>
+        </div>
+      </div>
+    </div>
+  );
+}
