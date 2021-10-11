c09da2c01a5f6828cd5ff2aecd00c2bf8fc28cae
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-11 20:59:14 +0100

    ADD basic footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,26 @@
+import styles from "../styles/Footer.module.scss";
+
 export default function Footer() {
-  return <div>by nau</div>;
+  return (
+    <>
+      <div className={styles.footer}>
+        <div>
+          <p className={styles.footerTitle}>Links</p>
+          <p>Github</p>
+          <p>Linkedin</p>
+          <p>Linkedin</p>
+        </div>
+        <div>
+          <p className={styles.footerTitle}> Support </p>
+          <p> email </p>
+          <p> phone </p>
+          <p> Privacy policy</p>
+        </div>
+      </div>
+      <div>
+        <p>Nau</p>
+        <p>c 2021</p>
+      </div>
+    </>
+  );
 }
