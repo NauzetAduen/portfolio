86dc167b5f841020cf175ce05a0cd0408139b8b3
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-13 21:30:56 +0100

    REMOVE extra text on hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,19 +4,10 @@ export default function Hero() {
   return (
     <div className={styles.dividedDiv}>
       <div className={styles.info}>
-        <blockquote className={styles.quote}>
-          <p>“The journey of a thousand miles begins with a single step”</p>
-          <p>– Lao-Tse</p>
-        </blockquote>
-
+        <p className={styles.name}>Nauzet Hernandez</p>
         <p className={styles.intro}>
-          Hello, I’m <span className={styles.name}>Nauzet</span>, a Software
-          Engineer from the Canary Islands. I’m focusing on the frontend side of
-          things.
-        </p>
-        <p className={styles.extraInfo}>
-          Thanks for visiting my portfolio, where I share my ideas, goals and
-          projects.
+          Hello, I’m Nau, a Software Engineer from the Canary Islands. I’m
+          focusing on the frontend side of things.
         </p>
       </div>
       <div></div>
