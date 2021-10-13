ef5a44823bdc06be41140f461009245b04e435ae
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-13 15:21:22 +0100

    ADD logo to navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,23 @@
 import styles from "../styles/Nav.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href="/">NAU</Link>
+          <Link href="/">
+            <div className={styles.logoDiv}>
+              <Image
+                className={styles.imageLogo}
+                src="/favicon.ico"
+                width="50px"
+                height="50px"
+              />
+              <p className={styles.textLogo}>NAU</p>
+            </div>
+          </Link>
         </li>
         <li>
           <Link href="/me">ME</Link>
