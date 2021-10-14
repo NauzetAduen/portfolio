49d08a51949f86eb1c7649d1fc9b56342e771c58
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-14 23:34:18 +0100

    Improve hero component and add poppins font

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <div className={styles.dividedDiv}>
       <div className={styles.info}>
-        <p className={styles.name}>Nauzet Hernandez</p>
+        <p className={styles.name}>Nauzet Hernández</p>
         <p className={styles.intro}>
-          Hello, I’m Nau, a Software Engineer from the Canary Islands. I’m
-          focusing on the frontend side of things.
+          Software Engineer from the Canary Islands. Focused on the Frontend
+          side of things. Flutter Advocate.
         </p>
       </div>
       <div></div>
