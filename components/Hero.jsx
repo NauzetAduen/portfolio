a4eb2b3de1d8133e513ae657a4740cb7ce92c6b5
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-15 00:02:08 +0100

    Add checkout svg and improvements to hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,16 +1,20 @@
 import styles from "../styles/Hero.module.scss";
-
+import Image from "next/image";
+import checkout from "../public/Checkout.svg";
 export default function Hero() {
   return (
     <div className={styles.dividedDiv}>
       <div className={styles.info}>
         <p className={styles.name}>Nauzet Hernández</p>
         <p className={styles.intro}>
-          Software Engineer from the Canary Islands. Focused on the Frontend
-          side of things. Flutter Advocate.
+          Software Engineer from the Canary Islands.
         </p>
+        <p className={styles.intro}>Focused on the Frontend side of things. </p>
+        <p className={styles.intro}>Flutter Advocate.</p>
       </div>
-      <div></div>
+      <div className={styles.heroImage}>
+        <Image src={checkout} width="1000vw" height="" />
+      </div>
     </div>
   );
 }
