67fb500d5838ad4fcbb41bbff2cc0ed18a4e1c06
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-18 21:46:41 +0100

    Fix padding in copyright

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
         </div>
         <div>
           <p className={styles.footerTitle}> Copyright </p>
-          <p>Nau Hdez &copy; 2021</p>
+          <p className={styles.footerCopyright}>Nau Hdez &copy; 2021</p>
           <Image src="/favicon.ico" width="50px" height="50px" />
         </div>
       </div>
