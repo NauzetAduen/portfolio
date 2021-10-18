506c9f9c85433fafbb9bb9dbfb8bbd3a220a0e14
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-18 21:37:23 +0100

    ADD basic privacy policy

---
+++ pages/privacy-policy.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+export default function privacyPolicy() {
+  return (
+    <>
+      {/* <Nav /> */}
+
+      <div>
+        <h1>Welcome to our Privacy Policy</h1>
+        <p>
+          Your privacy is critically important to us. It is my policy to respect
+          your privacy regarding any information I may collect while operating
+          my website. This Privacy Policy applies to nauzet.web.app.
+        </p>
+        <p>
+          I respect your privacy and am committed to protecting personally
+          identifiable information you may provide me through the Website. I
+          have adopted this privacy policy ("Privacy Policy") to explain what
+          information may be collected on my Website, how I use this
+          information, and under what circumstances I may disclose the
+          information to third parties.
+        </p>
+        <p>
+          This Privacy Policy applies only to information I collect through the
+          Website and does not apply to my collection of information from other
+          sources. This Privacy Policy, together with the Terms and conditions
+          posted on my Website, set forth the general rules and policies
+          governing your use of my Website. Depending on your activities when
+          visiting our Website, you may be required to agree to additional terms
+          and conditions.
+        </p>
+        Website Visitors Like most website operators, adedelivered.com collects
+        non-personally-identifying information of the sort that web browsers and
+        servers typically make available, such as the browser type, language
+        preference, referring site, and the date and time of each visitor
+        request. adedelivered.com's purpose in collecting non-personally
+        identifying information is to better understand how adedelivered.com's
+        visitors use its website. From time to time, adedelivered.com may
+        release non-personally-identifying information in the aggregate, e.g.,
+        by publishing a report on trends in the usage of its website.
+        adedelivered.com also collects potentially personally-identifying
+        information like Internet Protocol (IP) addresses for logged in users
+        and for users leaving comments on adedelivered.com blog posts.
+        adedelivered.com only discloses logged in user and commenter IP
+        addresses under the same circumstances that it uses and discloses
+        personally-identifying information as described below. Gathering of
+        Personally-Identifying Information Certain visitors to
+        adedelivered.com's websites choose to interact with adedelivered.com in
+        ways that require adedelivered.com to gather personally-identifying
+        information. The amount and type of information that adedelivered.com
+        gathers depends on the nature of the interaction. For example, we ask
+        visitors who sign up for a blog at http://adedelivered.com to provide a
+        username and email address. Security The security of your Personal
+        Information is important to us, but remember that no method of
+        transmission over the Internet, or method of electronic storage is 100%
+        secure. While we strive to use commercially acceptable means to protect
+        your Personal Information, we cannot guarantee its absolute security.
+        Advertisements Ads appearing on our website may be delivered to users by
+        advertising partners, who may set cookies. These cookies allow the ad
+        server to recognize your computer each time they send you an online
+        advertisement to compile information about you or others who use your
+        computer. This information allows ad networks to, among other things,
+        deliver targeted advertisements that they believe will be of most
+        interest to you. This Privacy Policy covers the use of cookies by
+        adedelivered.com and does not cover the use of cookies by any
+        advertisers. Links To External Sites Our Service may contain links to
+        external sites that are not operated by us. If you click on a third
+        party link, you will be directed to that third party's site. We strongly
+        advise you to review the Privacy Policy and terms and conditions of
+        every site you visit. We have no control over, and assume no
+        responsibility for the content, privacy policies or practices of any
+        third party sites, products or services. adedelivered.com uses Google
+        AdWords for remarketing adedelivered.com uses the remarketing services
+        to advertise on third party websites (including Google) to previous
+        visitors to our site. It could mean that we advertise to previous
+        visitors who haven't completed a task on our site, for example using the
+        contact form to make an enquiry. This could be in the form of an
+        advertisement on the Google search results page, or a site in the Google
+        Display Network. Third-party vendors, including Google, use cookies to
+        serve ads based on someone's past visits. Of course, any data collected
+        will be used in accordance with our own privacy policy and Google's
+        privacy policy. You can set preferences for how Google advertises to you
+        using the Google Ad Preferences page, and if you want to you can opt out
+        of interest-based advertising entirely by cookie settings or permanently
+        using a browser plugin. Protection of Certain Personally-Identifying
+        Information adedelivered.com discloses potentially
+        personally-identifying and personally-identifying information only to
+        those of its employees, contractors and affiliated organizations that
+        (i) need to know that information in order to process it on
+        adedelivered.com's behalf or to provide services available at
+        adedelivered.com's website, and (ii) that have agreed not to disclose it
+        to others. Some of those employees, contractors and affiliated
+        organizations may be located outside of your home country; by using
+        adedelivered.com's website, you consent to the transfer of such
+        information to them. adedelivered.com will not rent or sell potentially
+        personally-identifying and personally-identifying information to anyone.
+        Other than to its employees, contractors and affiliated organizations,
+        as described above, adedelivered.com discloses potentially
+        personally-identifying and personally-identifying information only in
+        response to a subpoena, court order or other governmental request, or
+        when adedelivered.com believes in good faith that disclosure is
+        reasonably necessary to protect the property or rights of
+        adedelivered.com, third parties or the public at large. If you are a
+        registered user of http://adedelivered.com and have supplied your email
+        address, adedelivered.com may occasionally send you an email to tell you
+        about new features, solicit your feedback, or just keep you up to date
+        with what's going on with adedelivered.com and our products. We
+        primarily use our blog to communicate this type of information, so we
+        expect to keep this type of email to a minimum. If you send us a request
+        (for example via a support email or via one of our feedback mechanisms),
+        we reserve the right to publish it in order to help us clarify or
+        respond to your request or to help us support other users.
+        adedelivered.com takes all measures reasonably necessary to protect
+        against the unauthorized access, use, alteration or destruction of
+        potentially personally-identifying and personally-identifying
+        information. Aggregated Statistics adedelivered.com may collect
+        statistics about the behavior of visitors to its website.
+        adedelivered.com may display this information publicly or provide it to
+        others. However, adedelivered.com does not disclose your
+        personally-identifying information. Cookies To enrich and perfect your
+        online experience, adedelivered.com uses "Cookies", similar technologies
+        and services provided by others to display personalized content,
+        appropriate advertising and store your preferences on your computer. A
+        cookie is a string of information that a website stores on a visitor's
+        computer, and that the visitor's browser provides to the website each
+        time the visitor returns. adedelivered.com uses cookies to help
+        adedelivered.com identify and track visitors, their usage of
+        adedelivered.com, and their website access preferences. adedelivered.com
+        visitors who do not wish to have cookies placed on their computers
+        should set their browsers to refuse cookies before using
+        adedelivered.com's websites, with the drawback that certain features of
+        adedelivered.com's websites may not function properly without the aid of
+        cookies. By continuing to navigate our website without changing your
+        cookie settings, you hereby acknowledge and agree to adedelivered.com's
+        use of cookies. Privacy Policy Changes Although most changes are likely
+        to be minor, adedelivered.com may change its Privacy Policy from time to
+        time, and in adedelivered.com's sole discretion. adedelivered.com
+        encourages visitors to frequently check this page for any changes to its
+        Privacy Policy. Your continued use of this site after any change in this
+        Privacy Policy will constitute your acceptance of such change. Credit &
+        Contact Informationot
+      </div>
+    </>
+  );
+}
