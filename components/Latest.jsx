9fd5c029ad2341748076516d7ee9def5db737bbf
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-23 18:38:02 +0100

    ADD new svg to the latest component

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -3,6 +3,8 @@ import stylesButton from "../styles/Button.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import Image from "next/image";
+import WebsiteDesign from "../public/WebsiteDesign.svg";
 
 import { memo } from "react";
 
@@ -12,28 +14,36 @@ function Latest() {
       <div>
         <p className={styles.title}>Latest creations</p>
       </div>
-      <div className={styles.creation}>
-        <div className={styles.subcreation}>
-          <div className={styles.placeholder}>Image</div>
+      <div className={styles.columns}>
+        <div className={styles.svgColumn}>
+          <Image src={WebsiteDesign} />
         </div>
-        <div className={styles.subcreationText}>
-          <p className={styles.creationTitle}>Code Challenge for ADE</p>
-          <p>
-            This challenge help me land a job as a Flutter developer in ADE.
-          </p>
-          <button className={stylesButton.button}>more</button>
+        <div className={styles.creationsColumn}>
+          <div className={styles.creation}>
+            <div className={styles.subcreation}>
+              <div className={styles.placeholder}>Image</div>
+            </div>
+            <div className={styles.subcreationText}>
+              <p className={styles.creationTitle}>Code Challenge for ADE</p>
+              <p>
+                This challenge help me land a job as a Flutter developer in ADE.
+              </p>
+              <button className={stylesButton.button}>more</button>
+            </div>
+          </div>
+          <div className={styles.creationReverse}>
+            <div className={styles.subcreationText}>
+              <p className={styles.creationTitle}>Portfolio</p>
+              <p>This project was made with NextJs as a learning experience.</p>
+              <button className={stylesButton.button}>more</button>
+            </div>
+            <div className={styles.subcreation}>
+              <div className={styles.placeholder}>Image</div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.creationReverse}>
-        <div className={styles.subcreationText}>
-          <p className={styles.creationTitle}>Portfolio</p>
-          <p>This project was made with NextJs as a learning experience.</p>
-          <button className={stylesButton.button}>more</button>
-        </div>
-        <div className={styles.subcreation}>
-          <div className={styles.placeholder}>Image</div>
-        </div>
-      </div>
+
       <div className={styles.seeMore}>
         <Link href="/projects">
           <button className={stylesButton.longButton}>
