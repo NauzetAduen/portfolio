b16a60a69c566090ecb385bf8a6b6ac0d5ce87f9
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-10-30 17:15:50 +0100

    Add nav

--- pages/me.jsx
+++ pages/me.jsx
@@ -1,3 +1,18 @@
+import Head from "next/head";
+import Nav from "../components/Nav";
+
 export default function me() {
-  return <div>me</div>;
+  return (
+    <>
+      <Head>
+        <title>Nauzet Hernández Portfolio</title>
+        <meta name="description" content="Portfolio by Nauzet Hernández" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <main>
+        <p>me</p>
+      </main>
+    </>
+  );
 }
