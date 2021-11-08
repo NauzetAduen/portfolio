7153ebe8b8fcf75a67f42e93ba57dbb591bd0c4f
Nauzet Hernandez <Nauzet@ADEdelivered.com>
2021-11-08 11:26:45 +0000

    Add conditional images source

--- components/home/Latest.jsx
+++ components/home/Latest.jsx
@@ -1,42 +1,42 @@
-import styles from "../../styles/Latest.module.scss";
-import stylesButton from "../../styles/Button.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
-import WebsiteDesign from "../../public/WebsiteDesign.svg";
-import CodeChallenge from "../../assets/code_challenge.png";
-import FlutterPortfolio from "../../assets/flutter_portfolio.png";
-
+import Link from "next/link";
 import { memo, useState } from "react";
 
+import CodeChallenge from "../../assets/code_challenge.png";
+import FlutterPortfolio from "../../assets/flutter_portfolio.png";
+import WebsiteDesign from "../../public/WebsiteDesign.svg";
+import stylesButton from "../../styles/Button.module.scss";
+import styles from "../../styles/Latest.module.scss";
+
 function Latest() {
-  const [show01, setShow01] = useState(false);
-  const [show02, setShow02] = useState(false);
+  const [showImage, setShowImage] = useState(WebsiteDesign);
+  // const [show02, setShow02] = useState(false);
   return (
     <div className={styles.latest}>
       <p className={styles.title}>Latest creations</p>
       <div className={styles.columns}>
         <div className={styles.svgColumn}>
-          <Image src={WebsiteDesign} />
+          <Image src={showImage} />
         </div>
         <div className={styles.creationsColumn}>
           <div
             className={styles.creation}
             onMouseEnter={() => {
-              setShow01(true);
+              setShowImage(CodeChallenge);
             }}
             onMouseLeave={() => {
-              setShow01(false);
+              setShowImage(WebsiteDesign);
             }}
           >
             <div className={styles.subcreation}>
               <div className={styles.digit}>01</div>
-              {show01 && (
+              {/* {show01 && (
                 <div className={styles.notHidden}>
                   <Image src={CodeChallenge} />
                 </div>
-              )}
+              )} */}
             </div>
             <div className={styles.subcreationText}>
               <p className={styles.creationTitle}>Code Challenge for ADE</p>
@@ -48,20 +48,20 @@ function Latest() {
           <div
             className={styles.creation}
             onMouseEnter={() => {
-              setShow02(true);
+              setShowImage(FlutterPortfolio);
             }}
             onMouseLeave={() => {
-              setShow02(false);
+              setShowImage(WebsiteDesign);
             }}
           >
             <div className={styles.subcreation}>
               <div className={styles.digit}>02</div>
               {/* {show02 && <div className={styles.notHidden}>02</div>} */}
-              {show02 && (
+              {/* {show02 && (
                 <div className={styles.notHidden}>
                   <Image src={FlutterPortfolio} />
                 </div>
-              )}
+              )} */}
             </div>
             <div className={styles.subcreationText}>
               <p className={styles.creationTitle}>Portfolio</p>
