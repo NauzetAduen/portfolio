92bd86b4d1753b511b67e2e548c0c7893650dc9d
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-11-14 20:12:25 +0000

    add main scaffold to blog

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,3 +1,19 @@
+import Head from "next/head";
+import Introduction from "../components/me/Introduction";
+import Personal from "../components/me/Personal";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
 export default function blog() {
-  return <div>Blog</div>;
+  return (
+    <>
+      <Head>
+        <title>Nauzet Hernández Portfolio</title>
+        <meta name="description" content="Portfolio by Nauzet Hernández" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <main>Blog</main>
+    </>
+  );
 }
