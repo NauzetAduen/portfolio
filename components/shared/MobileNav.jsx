d431a94e3000f7777c9fe375d2351a4d32bdd063
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-12-16 05:57:35 +0000

    Improve positioning and add icon to mobile nav

--- components/shared/MobileNav.jsx
+++ components/shared/MobileNav.jsx
@@ -4,25 +4,34 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles.mobileNav}>
-      <div className={styles.icon}>
-        {isOpen ? (
-          <FontAwesomeIcon
-            icon={faTimes}
-            size="2x"
-            onClick={() => setIsOpen(false)}
-          />
-        ) : (
-          <FontAwesomeIcon
-            icon={faBars}
-            size="2x"
-            onClick={() => setIsOpen(true)}
-          />
-        )}
+      <div className={styles.visible}>
+        <Link href="/">
+          <div className={styles.logoDiv}>
+            <Image src="/favicon.ico" width="50px" height="50px" />
+            <p className={styles.textLogo}>nau.</p>
+          </div>
+        </Link>
+        <div className={styles.icon}>
+          {isOpen ? (
+            <FontAwesomeIcon
+              icon={faTimes}
+              size="1x"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faBars}
+              size="1x"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
+        </div>
       </div>
 
       {isOpen && (
